54abc6767387b4e879c6008dedabcb40d2b5ffc3
Trevor Tanner <trevtanner@gmail.com>
2021-07-13 11:16:25 -0700

    Progess

--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -8,6 +8,11 @@ contract TokenFarm {
     DappToken public dappToken;
     DaiToken public daiToken;
 
+    address[] public stakers;
+    mapping(address => uint) public stakingBalance;
+    mapping(address => bool) public hasStaked;
+    mapping(address => bool) public isStaking;
+
     constructor(DappToken _dappToken, DaiToken _daiToken) public {
         dappToken = _dappToken;
         daiToken = _daiToken;
@@ -15,9 +20,24 @@ contract TokenFarm {
 
     // 1. Stake Tokens (Deposit)
     function stakeTokens(uint _amount) public {
+
         //Transfer Mock Dai tokens to this contract for staking
         daiToken.transferFrom(msg.sender, address(this), _amount);
+
+        //Update Staking balance
+        stakingBalance[msg.sender] = stakingBalance[msg.sender] + _amount;
+
+        // Add user to stakers array *only* if they haven't staked already
+        if(!hasStaked[msg.sender]) {
+            stakers.push(msg.sender);
+        }
+
+        // Update staking status
+        isStaking[msg.sender] = true;
+        hasStaked[msg.sender] = true;
     }
 
     // 2. Unstaking Tokens (Withdraw)
+
+    // 3. Issuing Tokens
 }
